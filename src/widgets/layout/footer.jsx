dca54628a1407ea6585bfa03d0781b04b7ff51a2
KosM <mpoukouvalas.kk@gmail.com>
2025-10-06 01:38:12 +0300

    update default props in footer

--- src/widgets/layout/footer.jsx
+++ src/widgets/layout/footer.jsx
@@ -46,13 +46,10 @@ export function Footer({ brandName, brandLink, routes }) {
 }
 
 Footer.defaultProps = {
-  brandName: "Creative Tim",
-  brandLink: "https://www.creative-tim.com",
+  brandName: "DryFil",
+  brandLink: "#",
   routes: [
-    { name: "Creative Tim", path: "https://www.creative-tim.com" },
-    { name: "About Us", path: "https://www.creative-tim.com/presentation" },
-    { name: "Blog", path: "https://www.creative-tim.com/blog" },
-    { name: "License", path: "https://www.creative-tim.com/license" },
+    { name: "", path: "#" },
   ],
 };
 
